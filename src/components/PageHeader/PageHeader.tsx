76aa6676717c1d256d070e452f5741687b054f3b
pooulad <86445458+pooulad@users.noreply.github.com>
2023-09-13 01:32:28 +0330

    start developing header section

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -1,13 +1,40 @@
+import { ICategoryRequest } from "../../types";
+import logo from "../../assets/images/loader.png";
+import { Link } from "react-router-dom";
 import "./styles/pageHeader.css";
+import { isLinkActive } from "../../utils/ActiveLink";
 
-export default function PageHeader() {
+type PageHeaderProps = {
+  categories: ICategoryRequest[];
+};
+
+export default function PageHeader(props: PageHeaderProps) {
+  console.log(props.categories);
+
   return (
-    <div>
-      <div>header</div>
-      <div>header</div>
-      <div>header</div>
-      <div>header</div>
-      <div>header</div>
+    <div className="header">
+      <img src={logo} alt="logo" />
+      <div className="header_bottom">
+        <div className="header_links">
+          {props.categories.map((item) => {
+            return (
+              <div key={item.id}>
+                <Link
+                  className={
+                    isLinkActive(`/category/${item.id}`)
+                      ? "active_header_link"
+                      : "header_link"
+                  }
+                  to={`/category/${item.id}`}
+                >
+                  {item.name}
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+        <div className="cart_div"></div>
+      </div>
     </div>
   );
 }
