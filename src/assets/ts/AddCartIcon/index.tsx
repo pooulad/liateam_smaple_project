3e69601d9a4733e395b3c097dad80a7b55770e2f
pooulad <86445458+pooulad@users.noreply.github.com>
2023-09-13 16:11:24 +0330

    add new icon in project

---
+++ src/assets/ts/AddCartIcon/index.tsx
@@ -0,0 +1,54 @@
+export default function AddCartIcon() {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_1_724)">
+        <path
+          d="M6.75012 16.5C7.16434 16.5 7.50012 16.1642 7.50012 15.75C7.50012 15.3358 7.16434 15 6.75012 15C6.33591 15 6.00012 15.3358 6.00012 15.75C6.00012 16.1642 6.33591 16.5 6.75012 16.5Z"
+          stroke="#212121"
+          stroke-width="1.25"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M14.9999 16.5C15.4141 16.5 15.7499 16.1642 15.7499 15.75C15.7499 15.3358 15.4141 15 14.9999 15C14.5857 15 14.2499 15.3358 14.2499 15.75C14.2499 16.1642 14.5857 16.5 14.9999 16.5Z"
+          stroke="#212121"
+          stroke-width="1.25"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M0.749878 0.749939H3.74988L5.75988 10.7924C5.82846 11.1377 6.01631 11.4479 6.29054 11.6687C6.56476 11.8894 6.9079 12.0067 7.25988 11.9999H14.5499C14.9019 12.0067 15.245 11.8894 15.5192 11.6687C15.7934 11.4479 15.9813 11.1377 16.0499 10.7924L17.2499 4.49994L17.2799 4.31988"
+          stroke="#212121"
+          stroke-width="1.25"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M10.8 2.15997L10.8 7.91997"
+          stroke="#212121"
+          stroke-width="1.25"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M7.92004 5.04004H13.68"
+          stroke="#212121"
+          stroke-width="1.25"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_1_724">
+          <rect width="18" height="18" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
