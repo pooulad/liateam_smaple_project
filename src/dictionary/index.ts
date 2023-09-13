4536e168c9ba4c10a578469c844dae801db7cc1a
pooulad <86445458+pooulad@users.noreply.github.com>
2023-09-13 10:02:08 +0330

    add footer contents in dictionary

--- src/dictionary/index.ts
+++ src/dictionary/index.ts
@@ -2,6 +2,12 @@ export const dictionary = {
   gettingAssets: "در حال بارگیری صفحه ...",
   footer: {
     social_text: "لیا را در شبکه‌های اجتماعی دنبال کنید",
+    subscribe_text: "لیا را در شبکه‌های اجتماعی دنبال کنید",
+    copyright: {
+      section1: "تمامی حقوق این وب‌سایت متعلق به شرکت",
+      section2: "آرمان تدبیر اطلس 1398-1400",
+      section3: "می‌باشد",
+    },
   },
   errors: {
     general: "خطایی رخ داد",
